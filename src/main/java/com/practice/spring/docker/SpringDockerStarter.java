846eb2d84e21efe22686904177ff340880e9696a
Avijit <avijit.haldia@gmail.com>
2019-07-12 00:27:53 +0530

    Initial Commit

---
+++ src/main/java/com/practice/spring/docker/SpringDockerStarter.java
@@ -0,0 +1,12 @@
+package com.practice.spring.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerStarter.class);
+    }
+}
